7fe56a7feca435ba68cfd06188e5929bed1f0a1a
lennox charles <lennyaiko17@gmail.com>
2023-10-02 16:23:59 +0100

    feat: add nav to homepage

--- assets/js/pages/index.vue
+++ assets/js/pages/index.vue
@@ -3,7 +3,19 @@
 </script>
 
 <template>
-    <div class="">
-        <p>This is a quiz app</p>
+    <div class="mx-auto my-0 max-w-3xl">
+
+        <div class="flex items-center my-2">
+
+            <p class="w-1/2">Quiz-App</p>
+
+            <div class="w-1/2 grid grid-cols-3 gap-4">
+
+                <a href="" class="mx-auto">Home</a>
+                <a href="" class="mx-auto">Add Questions</a>
+                <a href="" class="mx-auto">Take Quiz</a>
+
+            </div>
+        </div>
     </div>
 </template>
