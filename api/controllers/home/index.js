16a1c13c6fe10b1d5aae7342e2fb6975afca0d5d
lennox charles <lennyaiko17@gmail.com>
2023-10-02 13:37:15 +0100

    initial commit

---
+++ api/controllers/home/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  friendlyName: 'Index',
+
+  description: 'Index home.',
+
+  inputs: {  },
+
+  exits: {  },
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+};
